5e4e01d98f8123249bf7d1d859d3cc223e2196f1
AhmedShab <ahmed.vuw@gmail.com>
2016-06-29 18:44:08 +1200

    setup weather forecast api key to make requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '155a3b1ea43baad50689bcce9ae5629e';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FEATCH_WEATHER = 'FEATCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},nz`;
+  const request = axios.get(url);
+
+
+  return {
+    type: FEATCH_WEATHER,
+    payload: request
+  };
+}
