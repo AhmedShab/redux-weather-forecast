711c3bc1cfd0d7ce2d6bc83889f24eea5cfd1840
AhmedShab <ahmed.vuw@gmail.com>
2016-06-30 12:11:48 +1200

    google map displaying now

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {GoogleMapLoader, GoogleMap } from "react-google-maps";
+import { GoogleMapLoader, GoogleMap } from "react-google-maps";
 
 
 export default (props) =>{
   return (
-    <GoogleMapLoader containerElement= {<div style={{hieght: '100%'}} />} />
+    <GoogleMapLoader
+      containerElement={ <div style={{height: '100%'}} /> }
+      googleMapElement={
+        <GoogleMap defaultZoom={12} defaultCenter={{ lat: props.lat, lng: props.lon }} />
+      }
+      />
   );
-}
+};
