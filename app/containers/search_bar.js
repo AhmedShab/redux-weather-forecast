f80a6909e6ec7b02473328699c70aa71793122d5
AhmedShab <ahmed.vuw@gmail.com>
2016-07-02 16:41:51 +1200

    convert the temp from K to C

--- app/containers/search_bar.js
+++ app/containers/search_bar.js
@@ -32,7 +32,7 @@ class SearchBar extends Component {
       <form onSubmit={this.onFormSubmit.bind(this)} className="input-group">
         <input
           autoComplete="on"
-          placeholder="Get a five day forecast in your favorite city"
+          placeholder="Get a five day forecast in your favorite cities"
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange.bind(this)}/>
